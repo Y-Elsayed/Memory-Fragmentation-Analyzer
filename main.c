0c882e0b0c0af9254c0d5d03c7a246a83697941d
Potato-On-Fire <21-101128@students.eui.edu.eg>
2024-05-19 02:36:10 +0300

    Changed main to take input automatically.

--- main.c
+++ main.c
@@ -4,26 +4,14 @@
 #include "external_frag_analyzer.c"
 
 int main() {
-    // Test data
-    MemoryBlock blocks[] = {
-            {0, 100, true},
-            {100, 50, false},
-            {150, 80, true},
-            {230, 120, true},
-            {350, 70, false},
-            {420, 80, true},
-            {500, 50, true}
-    };
-
-    MemoryTable table = {100, false, 550, blocks, 7};
-
-    // Perform internal fragmentation analysis
-    printf("Test Case 1: Internal Fragmentation Analysis\n");
-    internal_frag_analyzer(&table);
-
-    // Perform external fragmentation analysis
-    printf("\nTest Case 2: External Fragmentation Analysis\n");
-    external_frag_analyzer(&table);
-
+    MemoryTable table = parse_memory_allocation();
+    if(table.is_fixed == true)
+    {
+        internal_frag_analyzer(&table);
+    }
+    else {
+        // Perform external fragmentation analysis
+        external_frag_analyzer(&table);
+    }
     return 0;
 }
